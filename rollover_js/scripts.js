502971c9e26fc6e26ed5468c8e5280e85ea918f6
Takazudo <takazudo@gmail.com>
2012-06-24 16:02:50 +0900

    tab to spaces

--- rollover_js/scripts.js
+++ rollover_js/scripts.js
@@ -1,14 +1,14 @@
 $(function(){
-	$('.rollover').each(function(){
-		var a = $(this);
-		var img = a.find('img');
-		var src_off = img.attr('src');
-		var src_on = src_off.replace('_off','_on');
-		$('<img />').attr('src', src_on);
-		a.hover(function(){
-			img.attr('src', src_on);
-		},function(){
-			img.attr('src', src_off);
-		});
-	});
+  $('.rollover').each(function(){
+    var a = $(this);
+    var img = a.find('img');
+    var src_off = img.attr('src');
+    var src_on = src_off.replace('_off','_on');
+    $('<img />').attr('src', src_on);
+    a.hover(function(){
+      img.attr('src', src_on);
+    },function(){
+      img.attr('src', src_off);
+    });
+  });
 });
